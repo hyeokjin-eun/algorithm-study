ba19e38122d1b229598c73a64df1c47cb3cd9ac9
hyeokjin-eun <gurwls2399@gmail.com>
2021-09-26 18:13:33 +0900

    feat : 알고리즘 공부 커밋

--- src/com/company/lecture/algorithm2/tree/Backjun1967.java
+++ src/com/company/lecture/algorithm2/tree/Backjun1967.java
@@ -4,27 +4,21 @@ import java.io.*;
 import java.util.*;
 
 // link
-// https://www.acmicpc.net/problem/1167
-public class Backjun1167 {
+// https://www.acmicpc.net/problem/1967
+public class Backjun1967 {
     private static final String[] array = {
-            "5\n" +
-            "1 3 2 -1\n" +
-            "2 4 4 -1\n" +
-            "3 1 2 4 3 -1\n" +
-            "4 2 4 3 3 5 6 -1\n" +
-            "5 4 6 -1",
-            "6\n" +
-            "1 2 3 -1\n" +
-            "2 1 3 5 3 3 5 -1\n" +
-            "3 2 5 4 7 -1\n" +
-            "4 3 7 -1\n" +
-            "5 2 3 6 5 -1\n" +
-            "6 5 5 -1",
-            "4\n" +
-            "1 2 7 3 2 -1\n" +
-            "2 1 7 -1\n" +
-            "3 1 2 4 3 -1\n" +
-            "4 3 3 -1"
+            "12\n" +
+            "1 2 3\n" +
+            "1 3 2\n" +
+            "2 4 5\n" +
+            "3 5 11\n" +
+            "3 6 9\n" +
+            "4 7 1\n" +
+            "4 8 7\n" +
+            "5 9 15\n" +
+            "5 10 4\n" +
+            "6 11 6\n" +
+            "6 12 10"
     };
 
     public static void main(String[] args) throws IOException {
@@ -45,38 +39,32 @@ public class Backjun1167 {
         OutputStreamWriter osw = new OutputStreamWriter(System.out);
         BufferedReader br = new BufferedReader(isr);
         BufferedWriter bw = new BufferedWriter(osw);
-        int v = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         ArrayList<ArrayList<Node>> a = new ArrayList<>();
-        for (int i = 0; i < v; i++) {
+        for (int i = 0; i < n; i++) {
             a.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < v; i++) {
+        for (int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken()) - 1;
-            while (true) {
-                int to = Integer.parseInt(st.nextToken()) - 1;
-                if (to == -2) {
-                    break;
-                }
-
-                int distance = Integer.parseInt(st.nextToken());
-                a.get(from).add(new Node(to, distance));
-            }
+            int to = Integer.parseInt(st.nextToken()) - 1;
+            int dist = Integer.parseInt(st.nextToken());
+            a.get(from).add(new Node(to, dist));
+            a.get(to).add(new Node(from, dist));
         }
 
-
         int root = 0;
-        int[] dist = bfs(root, a, v);
-        for (int i = 1; i < v; i++) {
+        int[] dist = bfs(root, n, a);
+        for (int i = 0; i< n; i++) {
             if (dist[i] > dist[root]) {
                 root = i;
             }
         }
 
-        dist = bfs(root, a, v);
+        dist = bfs(root, n, a);
         int answer = 0;
-        for (int i = 0; i < v; i++) {
+        for (int i = 0; i < n; i++) {
             if (answer < dist[i]) {
                 answer = dist[i];
             }
@@ -86,20 +74,20 @@ public class Backjun1167 {
         bw.flush();
     }
 
-    private static int[] bfs(int root, ArrayList<ArrayList<Node>> a, int v) {
-        boolean[] check = new boolean[v];
-        int[] dist = new int[v];
+    private static int[] bfs(int root, int n, ArrayList<ArrayList<Node>> a) {
+        int[] dist = new int[n];
+        boolean[] check = new boolean[n];
         Queue<Integer> q = new LinkedList<>();
         q.add(root);
         check[root] = true;
         while (!q.isEmpty()) {
-            int x  = q.poll();
+            int x = q.poll();
             for (int i = 0; i < a.get(x).size(); i++) {
-                Node nn = a.get(x).get(i);
-                if (!check[nn.c]) {
-                    dist[nn.c] = dist[x] + nn.d;
-                    check[nn.c] = true;
-                    q.add(nn.c);
+                Node node = a.get(x).get(i);
+                if (!check[node.c]) {
+                    dist[node.c] = dist[x] + node.d;
+                    check[node.c] = true;
+                    q.add(node.c);
                 }
             }
         }
@@ -110,7 +98,6 @@ public class Backjun1167 {
     private static class Node {
         int c;
         int d;
-
         public Node(int c, int d) {
             this.c = c;
             this.d = d;
