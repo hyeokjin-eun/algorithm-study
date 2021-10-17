85b192dc4e9cf93bdaf501379d187c92cb8671a0
hyeokjin-eun <gurwls2399@gmail.com>
2021-10-17 10:48:37 +0900

    feat : 알고리즘 공부 커밋

---
+++ src/com/company/greedy/Backjun12970.java
@@ -0,0 +1,37 @@
+package com.company.greedy;
+
+import java.io.*;
+import java.util.*;
+
+// link
+// https://www.acmicpc.net/problem/12970
+public class Backjun12970 {
+    private static final String[] array = {
+            "3 2"
+    };
+
+    public static void main (String[] args) throws IOException {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("===== Test Case " + i + " Start =====");
+            long before = System.currentTimeMillis();
+            solution(array[i]);
+            long after = System.currentTimeMillis();
+            System.out.println();
+            System.out.println("===== Time : " + (after - before) + "   =====");
+            System.out.println("===== Test Case " + i + " End   =====");
+        }
+    }
+
+    private static void solution(String input) throws IOException {
+        InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(input.getBytes()));
+        OutputStreamWriter osw = new OutputStreamWriter(System.out);
+        BufferedReader br = new BufferedReader(isr);
+        BufferedWriter bw = new BufferedWriter(osw);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        int a = 0;
+        int k = N;
+
+    }
+}
