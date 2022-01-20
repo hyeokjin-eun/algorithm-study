11eca17635fcfbd49b4a0cce6a8b8247137f4e1e
hyeokjin-eun <gurwls2399@gmail.com>
2022-01-20 23:19:14 +0900

    feat : studied the string
    - string algorithm problem number 2935

---
+++ src/com/company/string/Backjun2935.java
@@ -0,0 +1,53 @@
+package com.company.string;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+// link
+// https://www.acmicpc.net/problem/2935
+public class Backjun2935 {
+    private static final String[] array = {
+            "1000\n" +
+            "*\n" +
+            "100",
+            "10000\n" +
+            "+\n" +
+            "10"
+    };
+
+    public static void main (String[] args) throws IOException {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("===== Test Case " + i + " Start =====");
+            long before = System.currentTimeMillis();
+            solution(array[i]);
+            long after = System.currentTimeMillis();
+            System.out.println();
+            System.out.println("===== Time : " + (after - before) + "   =====");
+            System.out.println("===== Test Case " + i + " End   =====");
+        }
+    }
+
+    private static void solution(String input) throws IOException {
+        InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(input.getBytes()));
+        OutputStreamWriter osw = new OutputStreamWriter(System.out);
+        BufferedReader br = new BufferedReader(isr);
+        BufferedWriter bw = new BufferedWriter(osw);
+        BigInteger na = stoi(br.readLine());
+        char oper = br.readLine().charAt(0);
+        BigInteger nb = stoi(br.readLine());
+        BigInteger answer;
+        if (oper == '*') {
+            answer = na.multiply(nb);
+        } else {
+            answer = na.add(nb);
+        }
+
+        bw.write(answer.toString());
+        bw.flush();
+    }
+
+    private static BigInteger stoi(String s) {
+        return new BigInteger(s);
+    }
+}
