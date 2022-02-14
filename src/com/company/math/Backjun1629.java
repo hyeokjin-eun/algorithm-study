40ba21e0ed9948d624fadd9545af794c1bc90830
hyeokjin-eun <gurwls2399@gmail.com>
2022-02-14 15:36:46 +0900

    refactor : math algorithm problem number 1629
    - method segmentation
    - memoization add

--- src/com/company/math/Backjun1629.java
+++ src/com/company/math/Backjun1629.java
@@ -12,9 +12,11 @@ public class Backjun1629 {
     private static long A;
     private static long B;
     private static long C;
-    private static long answer = 1;
+    private static long answer;
+    private static final HashMap<Long, Long> memory = new HashMap<>();
     private static final String[] input = {
-            "10 11 12"
+            "10 11 12",
+            "4 1 2"
     };
 
     public static void main(String[] args) throws IOException {
@@ -32,7 +34,7 @@ public class Backjun1629 {
     private static void solution(String input) throws IOException {
         ioBuffered = new IOBuffered(input);
         setInputData();
-        modulus();
+        answer = recursion(B);
         printOutputData();
     }
 
@@ -47,10 +49,43 @@ public class Backjun1629 {
         return Integer.parseInt(s);
     }
 
-    private static void modulus() {
-        for (int i = 0; i < B; i++) {
-            answer = ((answer % C) * (A % C)) % C;
+    private static long recursion(long num) {
+        if (num == 1) {
+            return (A % C);
         }
+
+        if (isMemory(num)) {
+            return memory.get(num);
+        }
+
+        long result = getResult(num);
+        memory.put(num, result);
+        return result;
+    }
+
+    private static boolean isMemory(long num) {
+        return memory.containsKey(num);
+    }
+
+    private static long getResult(long num) {
+        long result = modular(num);
+        return arrangementRemain(num, result);
+    }
+
+    private static long modular(long num) {
+        return ((recursion(num / 2) % C) * (recursion(num / 2) % C)) % C;
+    }
+
+    private static long arrangementRemain(long num, long result) {
+        if (isOddNumber(num)) {
+            result = (result * A) % C;
+        }
+
+        return result;
+    }
+
+    private static boolean isOddNumber(long num) {
+        return num % 2 != 0;
     }
 
     private static void printOutputData() throws IOException {
