d787470661edc8c90776abbe560336a01de04093
hyeokjin-eun <gurwls2399@gmail.com>
2022-02-28 19:07:58 +0900

    feat : studying the divideandconquer algorithm
    - divideandconquer algorithm problem number 1992

---
+++ src/com/company/divideandconquer/Backjun1992.java
@@ -0,0 +1,105 @@
+package com.company.divideandconquer;
+
+import java.io.*;
+
+// link
+// https://www.acmicpc.net/problem/1992
+public class Backjun1992 {
+    private static IOBuffered ioBuffered;
+    private static int N;
+    private static int[][] video;
+    private static final String[] array = {
+            "8\n" +
+            "11110000\n" +
+            "11110000\n" +
+            "00011100\n" +
+            "00011100\n" +
+            "11110000\n" +
+            "11110000\n" +
+            "11110011\n" +
+            "11110011"
+    };
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("===== Test Case " + i + " Start =====");
+            long before = System.currentTimeMillis();
+            solution(array[i]);
+            long after = System.currentTimeMillis();
+            System.out.println();
+            System.out.println("===== Time : " + (after - before) + "   =====");
+            System.out.println("===== Test Case " + i + " End   =====");
+        }
+    }
+
+    private static void solution(String input) throws IOException {
+        ioBuffered = IOBuffered.create(input);
+        setInputData();
+        setAnswer();
+    }
+
+    private static void setInputData() throws IOException {
+        N = stoi(ioBuffered.readLine());
+        video = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            char[] chars = ioBuffered.readLine().toCharArray();
+            for (int j = 0; j < N; j++) {
+                video[i][j] = chars[j] - '0';
+            }
+        }
+    }
+
+    private static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    private static void setAnswer() {
+        compressionVideo();
+    }
+
+    private static void compressionVideo() {
+        StringBuilder sb = new StringBuilder();
+        recursion(sb, video, N);
+    }
+
+    private static void recursion(StringBuilder sb, int[][] temp, int length) {
+        if (temp.length == 1) {
+            sb.append(temp[0][0]);
+        }
+
+    }
+
+    private static class IOBuffered {
+        private final BufferedReader bufferedReader;
+        private final BufferedWriter bufferedWriter;
+
+        private IOBuffered(String input) {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(input.getBytes());
+            InputStreamReader inputStreamReader = new InputStreamReader(byteArrayInputStream);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(System.out);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+        }
+
+        public static IOBuffered create(String input) {
+            return new IOBuffered(input);
+        }
+
+        public String readLine() throws IOException {
+            return bufferedReader.readLine();
+        }
+
+        public void print(String answer) throws IOException {
+            write(answer);
+            flush();
+        }
+
+        private void write(String answer) throws IOException {
+            bufferedWriter.write(answer);
+        }
+
+        private void flush() throws IOException {
+            bufferedWriter.flush();
+        }
+    }
+}
