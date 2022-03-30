8c931bf1e0a073e5235c8e493f67a84af03c59b0
hyeokjin-eun <gurwls2399@gmail.com>
2022-03-30 23:45:37 +0900

    feat : studying the string algorithm
    - string algorithm problem number 1718

---
+++ src/com/company/string/Backjun1718.java
@@ -0,0 +1,87 @@
+package com.company.string;
+
+import com.company.math.Backjun1864;
+
+import java.io.*;
+import java.util.*;
+
+// link
+// https://www.acmicpc.net/problem/1718
+public class Backjun1718 {
+    private static final String[] array = {
+            "nice day\n" +
+            "love"
+    };
+    private static IOBuffered ioBuffered;
+    private static String str;
+    private static String key;
+    private static String \;
+
+    public static void main (String[] args) throws IOException {
+        // TEST
+        test();
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("===== Test Case " + i + " Start =====");
+            long before = System.currentTimeMillis();
+            solution(array[i]);
+            long after = System.currentTimeMillis();
+            System.out.println();
+            System.out.println("===== Time : " + (after - before) + "   =====");
+            System.out.println("===== Test Case " + i + " End   =====");
+        }
+    }
+
+    private static void test() {
+
+    }
+
+    private static void solution(String input) throws IOException {
+        ioBuffered = IOBuffered.create(input);
+        setData();
+        setAnswer();
+    }
+
+    private static void setData() throws IOException {
+        str = ioBuffered.readLine();
+        key = ioBuffered.readLine();
+    }
+
+    private static void setAnswer() {
+        String answer = null;
+        
+    }
+
+    private static class IOBuffered {
+        private final BufferedReader bufferedReader;
+        private final BufferedWriter bufferedWriter;
+
+        private IOBuffered(String input) {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(input.getBytes());
+            InputStreamReader inputStreamReader = new InputStreamReader(byteArrayInputStream);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(System.out);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+        }
+
+        public static IOBuffered create(String input) throws IOException {
+            return new IOBuffered(input);
+        }
+
+        public String readLine() throws IOException {
+            return bufferedReader.readLine();
+        }
+
+        public void print(String answer) throws IOException {
+            write(answer);
+            flush();
+        }
+
+        private void write(String answer) throws IOException {
+            bufferedWriter.write(answer);
+        }
+
+        private void flush() throws IOException {
+            bufferedWriter.flush();
+        }
+    }
+}
