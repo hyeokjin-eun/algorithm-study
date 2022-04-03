f91141aa2af3cc32e1aef28d515d46d528c70616
hyeokjin-eun <gurwls2399@gmail.com>
2022-04-03 18:15:46 +0900

    feat : studied the bf algorithm
    - bf algorithm problem number 4375

---
+++ go/bf/backjin4375.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var input =
+	"3\n" +
+	"7\n" +
+	"9901\n" +
+	"9999"
+
+func main() {
+	r := bufio.NewScanner(strings.NewReader(input))
+	w := bufio.NewWriter(os.Stdout)
+
+	var answer []int
+	for i := 0; ;i++ {
+		r.Scan()
+		s := r.Text()
+		if len(s) < 1 {
+			break
+		}
+
+		temp := 1
+		num,_ := strconv.Atoi(s)
+		count := 1
+		for temp % num != 0 {
+			temp = (temp * 10 + 1) % num
+			count++
+		}
+
+		answer = append(answer, count)
+	}
+
+	for i := 0; i < len(answer); i++ {
+		_, _ = w.WriteString(strconv.Itoa(answer[i]))
+		if i != len(answer) - 1 {
+			_, _ = w.WriteString("\n")
+		}
+	}
+
+	_ = w.Flush()
+}
---
+++ src/com/company/bf/Backjun4375_2.java
@@ -0,0 +1,120 @@
+package com.company.bf;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+// link
+// https://www.acmicpc.net/problem/4375
+public class Backjun4375_2 {
+    private static final String[] array = {
+            "3\n" +
+            "7\n" +
+            "9901\n" +
+            "9999"
+    };
+    private static IOBuffered ioBuffered;
+    private static List<Integer> inputData;
+    private static int[] answer;
+
+    public static void main (String[] args) throws IOException {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("===== Test Case " + i + " Start =====");
+            long before = System.currentTimeMillis();
+            solution(array[i]);
+            long after = System.currentTimeMillis();
+            System.out.println();
+            System.out.println("===== Time : " + (after - before) + "   =====");
+            System.out.println("===== Test Case " + i + " End   =====");
+        }
+    }
+
+    private static void solution(String input) throws IOException {
+        ioBuffered = IOBuffered.create(input);
+        setData();
+        for (int i = 0; i < inputData.size(); i++) {
+            setAnswer(i);
+        }
+
+        printAnswer();
+    }
+
+    private static void setData() throws IOException {
+        inputData = new ArrayList<>();
+        while (true) {
+            String s = ioBuffered.readLine();
+            if (s == null || s.isEmpty()) {
+                break;
+            }
+
+            inputData.add(stoi(s));
+        }
+
+        answer = new int[inputData.size()];
+    }
+
+    private static Integer stoi(final String s) {
+        return Integer.parseInt(s);
+    }
+
+    private static void setAnswer(final int target) {
+        int num = inputData.get(target);
+        int temp = 1;
+        if (num == 1) {
+            answer[target] = 1;
+            return;
+        }
+
+        int count = 1;
+        while (temp % num != 0) {
+            temp = (temp * 10 + 1) % num;
+            count++;
+        }
+
+        answer[target] = count;
+    }
+
+    private static void printAnswer() throws IOException {
+        ioBuffered.print(answer);
+    }
+
+    private static class IOBuffered {
+        private final BufferedReader bufferedReader;
+        private final BufferedWriter bufferedWriter;
+
+        private IOBuffered(String input) {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(input.getBytes());
+            InputStreamReader inputStreamReader = new InputStreamReader(byteArrayInputStream);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(System.out);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+        }
+
+        public static IOBuffered create(String input) {
+            return new IOBuffered(input);
+        }
+
+        public String readLine() throws IOException {
+            return bufferedReader.readLine();
+        }
+
+        public void print(int[] answer) throws IOException {
+            write(answer);
+            flush();
+        }
+
+        private void write(int[] answer) throws IOException {
+            for (int i = 0; i < answer.length; i++) {
+                bufferedWriter.write(String.valueOf(answer[i]));
+                if (i != answer.length - 1) {
+                    bufferedWriter.write("\n");
+                }
+            }
+        }
+
+        private void flush() throws IOException {
+            bufferedWriter.flush();
+        }
+    }
+}
